f983ca6ddee639c9b8fa2bc10314b6db31e8e536
andkymon <100330163+andkymon@users.noreply.github.com>
2024-11-27 13:44:11 +0800

    Add more books to myLibrary

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ function addBookToLibrary(title, author, genre, year) {
   myLibrary.push(new Book(title, author, genre, year));
 }
 
+
+
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "Fiction", 2000);
+addBookToLibrary("1984", "George Orwell", "Dystopian", 1949);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", "Romance", 1813);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1925);
+addBookToLibrary("Moby-Dick", "Herman Melville", "Adventure", 1851);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", "Fiction", 1951);
 console.log(myLibrary);
