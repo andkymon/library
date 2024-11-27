ea502db3996fb800942d60c4e8cb7ba973f1e944
andkymon <100330163+andkymon@users.noreply.github.com>
2024-11-27 14:14:36 +0800

    Add function to dynamically display books in table

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ function addBookToLibrary(title, author, genre, year) {
   myLibrary.push(new Book(title, author, genre, year));
 }
 
+function displayBook() {
+    const table = document.querySelector("table");
+    for (const index of myLibrary.keys()) {
+        const tableRow = document.createElement("tr");
+        const id = document.createElement("td");
+        const title = document.createElement("td");
+        const author = document.createElement("td");
+        const genre = document.createElement("td");
+        const year = document.createElement("td");
 
+        id.textContent = index + 1;
+        title.textContent = myLibrary[index].title;
+        author.textContent = myLibrary[index].author;
+        genre.textContent = myLibrary[index].genre;
+        year.textContent = myLibrary[index].year;
+
+        tableRow.append(id, title, author, genre, year);
+        table.append(tableRow);
+    }
+}
 
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "Fiction", 2000);
 addBookToLibrary("1984", "George Orwell", "Dystopian", 1949);
@@ -20,3 +39,4 @@ addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1925);
 addBookToLibrary("Moby-Dick", "Herman Melville", "Adventure", 1851);
 addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", "Fiction", 1951);
 console.log(myLibrary);
+displayBook();
